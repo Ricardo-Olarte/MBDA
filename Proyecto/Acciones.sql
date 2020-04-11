7436630cb93bf71c5f9afde402631e8bc3995cbb
Lfersar <luisafsar@gmail.com>
2020-04-11 15:12:48 -0500

    TG_AC

---
+++ Proyecto/Acciones.sql
@@ -0,0 +1,34 @@
+/*---------------------------------Mantener Plantilla---------------------------------*/
+/*Acciones de referencia*/
+ALTER TABLE Convocados DROP CONSTRAINT FK_Convocados_Plantillas;
+ALTER TABLE Convocados ADD CONSTRAINT FK_Convocados_Plantillas
+FOREIGN KEY (plantillaEquipo, plantillaPartido) REFERENCES Plantillas(equipo, partido) ON DELETE CASCADE;
+
+/*---------------------------------Mantener Estadio---------------------------------*/
+/*Acciones de referencia*/
+ALTER TABLE Equipos DROP CONSTRAINT FK_Equipos_Estadios;
+ALTER TABLE Equipos ADD CONSTRAINT FK_Equipos_Estadios 
+FOREIGN KEY (estadio) REFERENCES Estadios(nombre) ON DELETE CASCADE;
+
+ALTER TABLE Partidos DROP CONSTRAINT FK_Partidos_Estadios;
+ALTER TABLE Partidos ADD CONSTRAINT FK_Partidos_Estadios 
+FOREIGN KEY (estadio) REFERENCES Estadios(nombre) ON DELETE CASCADE;
+
+/*---------------------------------Registrar Partido---------------------------------*/
+/*Acciones de referencia*/encuentros
+
+ALTER TABLE Plantillas DROP CONSTRAINT FK_Plantilla_Partido;
+ALTER TABLE Plantillas ADD CONSTRAINT FK_Plantilla_Partido 
+FOREIGN KEY (partido) REFERENCES Partidos(partidoFecha) ON DELETE CASCADE;
+
+ALTER TABLE Eventos DROP CONSTRAINT FK_Eventos_Partido;
+ALTER TABLE Eventos ADD CONSTRAINT FK_Eventos_Partido 
+FOREIGN KEY (partido) REFERENCES Partidos(partidoFecha) ON DELETE CASCADE;
+
+ALTER TABLE PitadosPor DROP CONSTRAINT FK_PitadosPor_Partidos;
+ALTER TABLE PitadosPor ADD CONSTRAINT FK_PitadosPor_Partidos
+FOREIGN KEY (partido) REFERENCES Partidos(partidoFecha) ON DELETE CASCADE;
+
+ALTER TABLE Encuentros DROP CONSTRAINT FK_Encuentros_Partidos;
+ALTER TABLE Encuentros ADD CONSTRAINT FK_Encuentros_Partidos 
+FOREIGN KEY (partido) REFERENCES Partidos(partidoFecha) ON DELETE CASCADE;
---
+++ Proyecto/Disparadores.sql
@@ -0,0 +1,189 @@
+/*---------------------------------Mantener Jugador---------------------------------*/
+/*Disparadores*/
+--Adicionar
+CREATE OR REPLACE TRIGGER Tg_Ad_Jugador
+    BEFORE INSERT ON Jugadores
+    FOR EACH ROW
+    DECLARE
+        Cedula_C NUMBER(10);
+    BEGIN
+        SELECT Count(*)  INTO Cedula_C FROM Arbitros WHERE cedula = :new.cedula;
+        IF (Cedula_C > 0)
+            THEN RAISE_APPLICATION_ERROR(-20201,'LA CEDULA INGRESADA ESTA ASOCIADA A UN ARBITRO');
+        END IF;
+        IF (:new.precio is NULL)
+            THEN :new.precio := 0;
+        END IF;
+        IF (:new.lesion is NULL)
+            THEN :new.lesion := 0;
+        END IF;
+END Tg_Ad_Jugador;
+
+--Modificar
+CREATE OR REPLACE TRIGGER Tg_Mo_PersonasNaturales
+    BEFORE UPDATE ON PersonasNaturales
+    FOR EACH ROW
+    BEGIN
+        IF (:old.primerNombre <> :new.primerNombre) OR  (:old.primerApellido <> :new.primerApellido) 
+            OR (:old.cedula <> :new.cedula) OR (:old.nacionalidad <> :new.nacionalidad) 
+            OR (:old.fechaNacimiento <> :new.fechaNacimiento) OR (:new.rh <> :old.rh) OR (:new.sangre <> :old.sangre)
+		THEN
+			RAISE_APPLICATION_ERROR(-20202,'ACTUALIZACION NO PERMITIDA');
+	    END IF; 
+END Tg_Mo_PersonasNaturales;
+
+--Eliminar
+CREATE OR REPLACE TRIGGER Tg_El_Jugadores
+	BEFORE DELETE ON Jugadores
+	FOR EACH ROW
+	BEGIN
+		RAISE_APPLICATION_ERROR(-20203,'NO SE PUEDE ELIMINAR UN JUGADOR');
+END Tg_El_Jugadores;
+
+
+/*---------------------------------Mantener Plantilla---------------------------------*/
+/*Disparadores*/
+--Adicionar
+CREATE OR REPLACE TRIGGER Tg_Ad_Convocado
+    BEFORE INSERT ON Convocados
+    FOR EACH ROW
+    DECLARE
+        Convocados_T NUMBER(10);
+        Convocados_S NUMBER(10);
+        Convocados_PO NUMBER(10);
+    BEGIN
+        SELECT Count(*)  INTO Convocados_T 
+        FROM Convocados 
+        WHERE :new.plantillaEquipo = plantillaEquipo AND :new.plantillaPartido = plantillaPartido AND titular = 1;
+        
+        SELECT Count(*)  INTO Convocados_S 
+        FROM Convocados 
+        WHERE :new.plantillaEquipo = plantillaEquipo AND :new.plantillaPartido = plantillaPartido AND titular = 0;
+        
+        SELECT Count(*)  INTO Convocados_PO 
+        FROM Convocados 
+        WHERE :new.plantillaEquipo = plantillaEquipo AND :new.plantillaPartido = plantillaPartido AND titular = 1 AND posicion = 'POR';
+        
+        IF (:new.titular = 1 AND Convocados_T >= 11)
+            THEN RAISE_APPLICATION_ERROR(-20204,'CAPACIDAD DE JUGADORES TITULARES ALCANZADA');
+        END IF;
+        
+        IF (:new.titular = 0 AND Convocados_S >= 7)
+            THEN RAISE_APPLICATION_ERROR(-20205,'CAPACIDAD DE JUGADORES SUPLENTES ALCANZADA');
+        END IF;
+        
+        IF (Convocados_PO >= 1 AND :new.posicion = 'POR')
+            THEN RAISE_APPLICATION_ERROR(-20206,'CAPACIDAD DE PORTEROS TITULARES ALCANZADA');
+        END IF;
+END Tg_Ad_Convocado;
+
+--Modificar
+CREATE OR REPLACE TRIGGER Tg_Mo_Plantilla
+    BEFORE UPDATE ON Plantillas
+    FOR EACH ROW
+    BEGIN
+        IF (:old.equipo <> :new.equipo) OR  (:old.partido <> :new.partido)
+		THEN
+			RAISE_APPLICATION_ERROR(-20206,'ACTUALIZACION NO PERMITIDA');
+	    END IF; 
+END Tg_Mo_Plantilla;
+
+
+/*---------------------------------Mantener Equipo---------------------------------*/
+/*Disparadores*/ 
+--Adicionar
+CREATE OR REPLACE TRIGGER Tg_Ad_Equipo
+    BEFORE INSERT ON Equipos
+    FOR EACH ROW
+    BEGIN
+        IF (:new.pais is NULL)
+            THEN :new.pais := 'Colombia';
+        END IF;
+        IF (:new.apodo is NULL)
+            THEN :new.apodo := (:new.nombre || ' De ' || :new.pais);
+        END IF;
+END Tg_Ad_Equipo;
+
+--Modificar
+CREATE OR REPLACE TRIGGER Tg_Mo_Equipo
+    BEFORE UPDATE ON Equipos
+    FOR EACH ROW
+    BEGIN
+        IF (:old.nombre <> :new.nombre) OR (:old.pais <> :new.pais) OR (:old.fechaFundacion <> :new.fechaFundacion)
+            OR (:old.ciudad <> :new.ciudad)
+		THEN
+			RAISE_APPLICATION_ERROR(-20207,'ACTUALIZACION NO PERMITIDA');
+	    END IF; 
+END Tg_Mo_Equipo;
+
+--Eliminar
+CREATE OR REPLACE TRIGGER Tg_El_Equipos
+	BEFORE DELETE ON Equipos
+	FOR EACH ROW
+	BEGIN
+		RAISE_APPLICATION_ERROR(-20208,'NO SE PUEDE ELIMINAR UN EQUIPO');
+END Tg_El_Equipos;
+
+
+/*---------------------------------Mantener Estadio---------------------------------*/
+/*Disparadores*/
+--Adicionar
+CREATE OR REPLACE TRIGGER Tg_Ad_Estadio
+    BEFORE INSERT ON Estadios
+    FOR EACH ROW
+    BEGIN
+        IF (:new.pais is NULL)
+            THEN :new.pais := 'Colombia';
+        END IF;
+        IF (:new.inauguracion is NULL)
+            THEN :new.inauguracion := SYSDATE();
+        END IF;
+END Tg_Ad_Estadio;
+
+--Modificar capacidad y nombre
+CREATE OR REPLACE TRIGGER Tg_Mo_Estadio
+    BEFORE UPDATE ON Estadios
+    FOR EACH ROW
+    BEGIN
+        IF (:old.pais <> :new.pais) OR (:old.ubicacion <> :new.ubicacion) OR (:old.inauguracion <> :new.inauguracion)
+           OR (:old.nombre <> :new.nombre)
+		THEN RAISE_APPLICATION_ERROR(-20209,'ACTUALIZACION NO PERMITIDA');
+	    END IF; 
+END Tg_Mo_Estadio;
+
+
+/*---------------------------------Registrar Partido---------------------------------*/
+/*Disparadores*/
+--Adicionar
+CREATE OR REPLACE TRIGGER Tg_Ad_Partido
+    BEFORE INSERT ON Partidos
+    FOR EACH ROW
+    BEGIN
+        IF (:new.partidoFecha > (SYSDATE() + 365))
+            THEN RAISE_APPLICATION_ERROR(-20210,'FECHA SUPERIOR A UN AÑO');
+        END IF;
+END Tg_Ad_Partido;
+
+
+INSERT INTO Partidos (partidoFecha, marcadorFinal, estadio)
+VALUES (TO_DATE('25-01-2022 22:00','DD-MM-YYYY HH24:MI'), '1-2', 'Estadio Nemesio Camacho El Campin');
+
+--Modificar
+CREATE OR REPLACE TRIGGER Tg_Mo_Partido
+    BEFORE UPDATE ON Partidos
+    FOR EACH ROW
+    BEGIN
+        IF (:old.marcadorFinal is NOT NULL)
+    		THEN RAISE_APPLICATION_ERROR(-20211,'ACTUALIZACION NO PERMITIDA, IMPOSBILE CAMBIAR MARCADOR');
+	    END IF; 
+END Tg_Mo_Partido;
+
+--Eliminar
+CREATE OR REPLACE TRIGGER Tg_El_Partido
+	BEFORE DELETE ON Partidos
+	FOR EACH ROW
+	BEGIN
+        IF (:old.marcadorFinal is NOT NULL)
+        	THEN RAISE_APPLICATION_ERROR(-20211,'ELIMINACION NO PERMITIDA');
+	    END IF; 
+END Tg_El_Partido;
