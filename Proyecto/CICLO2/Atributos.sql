09fdc7234452575ec33a4ef8230bb220925f5990
mateo <mateo.cor.amco@gmail.com>
2020-05-25 11:05:23 -0500

    ACT

--- Proyecto/CICLO2/Atributos.sql
+++ Proyecto/CICLO2/Atributos.sql
@@ -11,14 +11,17 @@ CHECK (contrato IN ('P','V'));
 ALTER TABLE Fisioterapeutas ADD CONSTRAINT CK_Fisioterapeutas_Disciplina 
 CHECK (disciplina IN ('LH','LR','LT','LC'));
 
+ALTER TABLE Fisioterapeutas ADD CONSTRAINT CK_Fisioterapeutas_salario
+CHECK (salario>0);
+
 ALTER TABLE Presidente ADD CONSTRAINT CK_Presidente_vecesElec
 CHECK (vecesElecto>0);
 
 ALTER TABLE Descensos ADD CONSTRAINT CK_Descensos_liga 
 CHECK (liga IN ('B','C'));
 
-ALTER TABLE Fisioterapeutas ADD CONSTRAINT CK_Fisioterapeutas_salario
-CHECK (salario>0);
-
 ALTER TABLE DirectoresTecnicos ADD CONSTRAINT CK_DirectoresTecnicos_sueldo
 CHECK (sueldo>0);
+
+ALTER TABLE Terapias ADD CONSTRAINT CK_Terapias_duracion
+CHECK (duracion>20 AND duracion<240);
